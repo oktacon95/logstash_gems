a03b889d11b478a67f0ca11bca5483719f87176a
Metzger Manuel <Manuel.METZGER@lbs4.salzburg.at>
2018-10-13 16:57:08 +0200

    initial commit

---
+++ logstash-filter-cpu_temp/lib/logstash/filters/cpu_temp.rb
@@ -0,0 +1,51 @@
+# filter for c2 server out file (but the one with the naming context in the filename -- has date information included!)
+require "logstash/filters/base"
+require "logstash/namespace"
+
+class LogStash::Filters::CPU_TEMP < LogStash::Filters::Base
+
+  config_name "cpu_temp"
+
+  # New plugins should start life at milestone 1.
+  milestone 1
+
+  def register
+    # nothing to do
+  end # def register
+
+  def filter(event)
+    # return nothing unless there's an actual filter event
+    return unless filter?(event)
+	
+	originalmessage = event.get('message')
+	
+	# 12-10-2018 11:26:58 32.968
+	# Trying to match the timestamp
+	if /^([\d]{2}-[\d]{2}-[\d]{4}\s[\d]{2}:[\d]{2}:[\d]{2})\s)([^*]+)$/.match(event.get('message'))
+		
+		begin
+			rubytime = Time.strptime($1, "%d-%m-%Y %H:%M:%S")
+			logstash_time = LogStash::Timestamp.new(rubytime)
+			event.set('@timestamp', logstash_time)
+		rescue Exception => e  
+			event.set('debuginfo', 'Failed to parse date <' + $1 + '>')
+			@logger.warn('Failed to parse date <' + $1 + '>; message: ' + e.message + ', stacktrace:' + e.backtrace.inspect)
+		end
+		
+		event.set('message', $2)
+		# Trying to match the temperature
+		if /^([\d]+\.[\d]+)$/.match(event.get('message')
+			event.set('temperature', $1.to_f)
+		else
+			event.set('debuginfo', "cannot read cpu temperature")
+		end
+	else
+		event.set('debuginfo', "cannot read timestamp, bad format")
+	end
+	
+	event.set('message', originalmessage)
+	
+    filter_matched(event)
+	
+  end # def filter
+end # class LogStash::Filters::CPU_TEMP
---
+++ logstash-filter-cpu_temp/spec/filters/cpu_temp.rb
@@ -0,0 +1,67 @@
+# encoding: utf-8
+require_relative '../spec_helper'
+require "logstash/filters/cpu_temp"
+
+describe LogStash::Filters::CPU_TEMP do
+  subject(:plugin) { LogStash::Filters::CPU_TEMP.new (config) }
+  let(:config) { Hash.new }
+
+  let(:doc) { "" }
+  let(:event) { LogStash::Event.new("message" => doc) }
+
+  before(:each) do
+    plugin.register
+  end
+  
+  describe "Positive Tests - " do
+
+	describe "filter one complete message - " do
+		
+		let(:doc) { "12-10-2018 11:27:02 32.999" }
+
+		it "should extract timestamp and cpu temperature" do
+			plugin.filter(event)
+			expect(event.get("@timestamp").to_f).to eq(1492463445.876)
+			expect(event.get("cputemperature")).to eq(32.999)
+			expect(event.get("message")).to eq("12-27-2018 11:27:02 32.999")
+		end
+	end
+
+	describe "filter another complete message - " do
+		let(:doc) { "24-12-2018 22:58:42 0.321" } 
+		
+		it "should extract timestamp" do
+			plugin.filter(event)
+			expect(event.get("@timestamp").to_f).to eq(1492553570.878)
+			expect(event.get("cputemperature")).to eq(0.321)
+			expect(event.get("message")).to eq("24-12-2018 11:27:02 0.321")
+		end
+	end 
+  end # end Positive Tests
+  
+  describe "Negative Tests - " do
+
+	describe "filter a wrong date time format - " do
+		
+		let(:doc) { "12-50-2018 11:27:02 32.999" }
+
+		it "set a debuginfo field" do
+			plugin.filter(event)
+			expect(event.get("debuginfo")).to eq("Failed to parse date <12-50-2018>")
+			expect(event.get("message")).to eq("12-50-2018 11:27:02 32.999")
+		end
+	end
+
+	describe "filter a wrong formatted cpu temperature - " do
+		
+		let(:doc) { "12-10-2018 11:27:02 32,999" }
+
+		it "set a debuginfo field" do
+			plugin.filter(event)
+			expect(event.get("debuginfo")).to eq("cannot read cpu temperature")
+			expect(event.get("message")).to eq("12-12-2018 11:27:02 32,999")
+		end
+	end
+  end # end Negative Tests
+
+end # LogStash::Filters::CPU_TEMP
